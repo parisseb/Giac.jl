727ff4dc4a4a2602aa6795187a3fd5ea7d395fc0
parisseb <parisseb@orange.fr>
2022-10-15 11:52:20 +0200

    fix segfault at libgiac startup

--- deps/src/giac_c.C
+++ deps/src/giac_c.C
@@ -1,6 +1,6 @@
 // http://stackoverflow.com/questions/2045774/developing-c-wrapper-api-for-object-oriented-c-code
 
-
+#include "giac/config.h"
 #include "giac/giac.h"
 #include "giac_c.h"
 #include <string>
